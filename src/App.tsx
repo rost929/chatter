bb120712cc4854d3a844ece291fdced365a29e3b
jpena <juan.pena@wmcontractor.com>
2024-06-24 18:31:26 -0500

    React App initialization

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Container, CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark'
+  }
+})
+
+const App = () => {
+  return (<div>
+    <ThemeProvider theme={darkTheme}>
+        <CssBaseline/>
+        <Container>
+          <h1>Dark Mode</h1>
+        </Container>
+    </ThemeProvider>
+  </div>)
 }
 
 export default App;
